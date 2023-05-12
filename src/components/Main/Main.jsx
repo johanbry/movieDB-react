c5acc6954ae23b83d2a851419140f49485633505
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-12 13:20:48 +0200

    added components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,16 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import MovieDetails from "./MovieDetails/MovieDetails";
+import MovieList from "./MovieList/MovieList";
+
 const Main = () => {
   return (
     <div>
       <h1>Main</h1>
+      <MovieList />
+      <MovieDetails />
+      <About />
+      <Contact />
     </div>
   );
 };
