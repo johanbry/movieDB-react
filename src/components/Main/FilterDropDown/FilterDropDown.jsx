ae59ccca44b84ea31ebdc4b54b0aa14dce94112c
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-16 15:17:44 +0200

    added state and callback to update movies list

--- src/components/Main/FilterDropDown/FilterDropDown.jsx
+++ src/components/Main/FilterDropDown/FilterDropDown.jsx
@@ -1,11 +1,31 @@
-const FilterDropDown = () => {
+import { useState, useEffect } from "react";
+
+const FilterDropDown = ({ handleFilterChange }) => {
+  const [category, setCategory] = useState("popular");
+
+  // const handleChange = (selectedCategory) => {
+  //   setCategory(selectedCategory);
+  // };
+
+  useEffect(() => {
+    handleFilterChange(category);
+  }, [category, handleFilterChange]);
+
   return (
-    <select>
+    <select onChange={(e) => setCategory(e.target.value)}>
       <option value="popular">Popular</option>
       <option value="top_rated">Top Rated</option>
-      <option value="uopcoming">Upcoming</option>
+      <option value="upcoming">Upcoming</option>
     </select>
   );
 };
 
 export default FilterDropDown;
+
+// printSelectedValue = () => {
+//   console.log(this.state.select);
+// }
+
+// select = (event) => {
+//   this.setState({ select: event.target.value }, this.printSelectedValue)
+// };
