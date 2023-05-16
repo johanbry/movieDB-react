cab7f7ac8a3bc4ebbd846b17b26ec9c76f9b3123
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-16 15:25:21 +0200

    fetch and render list of movies

--- src/components/Main/MovieList/MovieList.jsx
+++ src/components/Main/MovieList/MovieList.jsx
@@ -1,12 +1,47 @@
+import useFetch from "../../../hooks/useFetch";
+import { useState, useCallback } from "react";
 import FilterDropDown from "../FilterDropDown/FilterDropDown";
 import MovieCard from "../MovieCard/MovieCard";
+import "./movielist.css";
 
 const MovieList = () => {
+  const defaultUrl =
+    import.meta.env.VITE_API_URL +
+    "popular?api_key=" +
+    import.meta.env.VITE_API_KEY;
+
+  const [url, setUrl] = useState(defaultUrl);
+
+  const { data: movies, isLoading, errorMessage } = useFetch(url);
+  //console.log(movies, isLoading, errorMessage);
+
+  const handleFilterChange = useCallback((category) => {
+    console.log("handleFilter parent", category);
+    setUrl(
+      import.meta.env.VITE_API_URL +
+        category +
+        "?api_key=" +
+        import.meta.env.VITE_API_KEY
+    );
+  }, []);
+  //(category) => {
+
+  //};
+
+  //api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
   return (
     <div>
-      <h1>MovieList</h1>
-      <FilterDropDown />
-      <MovieCard />
+      <FilterDropDown handleFilterChange={handleFilterChange} />
+      {isLoading && <p>Laddar...</p>}
+      {errorMessage && <p>Något gick fel</p>}
+      {/* <section>{movies && <MovieCard movies={movies} />}</section> */}
+
+      <section className="movie-grid">
+        {movies &&
+          movies.results.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+      </section>
     </div>
   );
 };
