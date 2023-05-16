75472c3e330aa25d6202db23fc9c3ddf3546bb6c
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-16 15:13:20 +0200

    added Routes to all components/pages

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import About from "./About/About";
 import Contact from "./Contact/Contact";
 import MovieDetails from "./MovieDetails/MovieDetails";
 import MovieList from "./MovieList/MovieList";
+import Actor from "./Actor/Actor";
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
-      <MovieList />
-      <MovieDetails />
-      <About />
-      <Contact />
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path=":id" element={<MovieDetails />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="actor/:id" element={<Actor />} />
+      </Routes>
+    </main>
   );
 };
 
