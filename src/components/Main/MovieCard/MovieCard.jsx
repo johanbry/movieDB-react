2578d0cbc1d08760edf6ccda891f3481f5bade44
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-16 15:20:06 +0200

    render movie card with link to details component

--- src/components/Main/MovieCard/MovieCard.jsx
+++ src/components/Main/MovieCard/MovieCard.jsx
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom";
 import MarkAsSeenBtn from "../MarkAsSeenBtn/MarkAsSeenBtn";
+import "./moviecard.css";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+  const imgUrl = "https://image.tmdb.org/t/p/w500/";
   return (
-    <div>
-      <h1>MovieCard</h1>
-      <MarkAsSeenBtn />
-    </div>
+    <>
+      <article className="gradient">
+        <Link to={movie.id.toString()}>
+          <img
+            src={imgUrl + movie.poster_path}
+            alt={movie.title}
+            style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+          />
+          <div className="movie-details">
+            <h3>{movie.title}</h3>
+            <MarkAsSeenBtn />
+          </div>
+        </Link>
+      </article>
+    </>
   );
 };
 export default MovieCard;
